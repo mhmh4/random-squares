5f56dae6dc037106cf36963644e88b85683d9094
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-05 17:27:17 -0400

    Create 2D array to store colors

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ const n = canvas.clientHeight / PIXEL_SIZE;
 console.log(canvas.clientWidth, "->", m);
 console.log(canvas.clientHeight, "->", n);
 
+const grid = [];
+
+for (let i = 0; i < m; i++) {
+  const row = [];
+  for (let j = 0; j < n; j++) {
+    row.push("#fff");
+  }
+  grid.push(row);
+}
+console.log(grid);
+
 for (let x = 0; x < canvas.clientWidth; x += PIXEL_SIZE) {
   for (let y = 0; y < canvas.clientHeight; y += PIXEL_SIZE) {
     ctx.strokeRect(x, y, x + PIXEL_SIZE, y + PIXEL_SIZE);
