3c1067d540d11903a4db56319562da10dcedf13a
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-05 16:51:40 -0400

    Change canvas dimensions

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 900;
+canvas.height = 300;
+
 console.log(canvas);
 
 const PIXEL_SIZE = 25;
