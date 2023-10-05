4b4a2677d1121104369c67003179a1d4e7482941
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-05 15:22:03 -0400

    Outline a grid within the canvas

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 console.log(canvas);
+
+const PIXEL_SIZE = 25;
+
+const m = canvas.clientWidth / PIXEL_SIZE;
+const n = canvas.clientHeight / PIXEL_SIZE;
+
+console.log(canvas.clientWidth, "->", m);
+console.log(canvas.clientHeight, "->", n);
+
+for (let x = 0; x < canvas.clientWidth; x += PIXEL_SIZE) {
+  for (let y = 0; y < canvas.clientHeight; y += PIXEL_SIZE) {
+    ctx.strokeRect(x, y, x + PIXEL_SIZE, y + PIXEL_SIZE);
+  }
+}
