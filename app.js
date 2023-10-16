16715748b41eb697a47e9379585bb362b4bec99f
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-15 22:25:10 -0400

    Reset app.js to outlining a grid

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const CANVAS_WIDTH = 800;
+const CANVAS_WIDTH = 1000;
 const CANVAS_HEIGHT = 500;
+
+const PIXEL_SIZE = 20;
+const SCALE = 2;
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-const PIXEL_SIZE = 20;
-
-const SCALE = 2;
 
 canvas.style.width = CANVAS_WIDTH + "px";
 canvas.style.height = CANVAS_HEIGHT + "px";
@@ -14,54 +13,13 @@ canvas.style.height = CANVAS_HEIGHT + "px";
 canvas.width = CANVAS_WIDTH * SCALE;
 canvas.height = CANVAS_HEIGHT * SCALE;
 
-const m = canvas.width / PIXEL_SIZE;
-const n = canvas.height / PIXEL_SIZE;
+function draw(ctx, canvas, pixelSize) {
+  const rows = canvas.width;
+  const cols = canvas.height;
 
-let palette = ["#21295C", "#1B3B6F", "#065A82", "#1C7293", "#304C89"];
-const grid = createGrid(m, n, "#fff");
-
-const inputs = document.querySelectorAll(".palette-color");
-const previews = document.querySelectorAll(".color-preview");
-
-const NUM_ITERATIONS = 20000;
-const lengths = [5, 4, 3, 2, 1];
-
-const generateButton = document.getElementById("generate-button");
-
-function createGrid(rows, cols, val) {
-  const output = [];
-  for (let i = 0; i < rows; i++) {
-    const row = [];
-    for (let j = 0; j < cols; j++) {
-      row.push(val);
-    }
-    output.push(row);
-  }
-  return output;
-}
-
-function drawGrid(ctx, grid, pixelSize) {
-  const rows = grid.length;
-  const cols = grid[0].length;
-
-  let x = 0;
-  let y = 0;
-
-  for (let j = 0; j < cols; j++) {
-    x = 0;
-    for (let i = 0; i < rows; i++) {
-      ctx.fillStyle = grid[i][j];
-      ctx.fillRect(x, y, pixelSize, pixelSize);
-      x += pixelSize;
-    }
-    y += pixelSize;
-  }
-}
-
-function fill(grid, i1, j1, i2, j2, color) {
-  for (let x = i1; x <= i2; x++) {
-    for (let y = j1; y <= j2; y++) {
-      grid[x][y] = color;
+  for (let i = 0; i < rows; i += pixelSize) {
+    for (let j = 0; j < cols; j += pixelSize) {
+      ctx.strokeRect(i, j, pixelSize, pixelSize);
     }
   }
 }
@@ -77,22 +35,4 @@ function choice(array) {
   return array[randomIndex];
 }
 
-function update() {
-  for (let i = 0; i < NUM_ITERATIONS; i++) {
-    const length = choice(lengths);
-
-    let x = getRandomIntInclusive(0, grid.length - 1 - length);
-    let y = getRandomIntInclusive(0, grid[0].length - 1 - length);
-
-    let x2 = x + length;
-    let y2 = y + length;
-
-    fill(grid, x, y, x2, y2, choice(palette));
-  }
-
-  drawGrid(ctx, grid, PIXEL_SIZE);
-}
-
-window.addEventListener("load", () => {
-  update();
-});
+draw(ctx, canvas, PIXEL_SIZE);
